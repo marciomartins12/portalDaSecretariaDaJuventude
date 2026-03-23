9b9b5bfe90477e66125c4b6eb14738905629df1d
marciomartins12 <mmarciom1martins635@gmail.com>
2026-03-23 11:57:04 -0300

    lista de sorteados no trabalho jovem

--- services/trabalhoJovemService.js
+++ services/trabalhoJovemService.js
@@ -18,7 +18,26 @@ const trabalhoJovem = {
     title: "Selecionados Trabalho Jovem",
     placeholder: "Buscar nome ou CPF…",
     rows: [
-      { name: "Pessoa Selecionada", cpf: "000.000.000-00" }
+      { name: "ANDRESSA CRISTINA DE JESUS FREITAS", cpf: "618.xxx.xxx-**" },
+      { name: "BIANCA VITORIA RIBEIRO DOS INOCENTES ", cpf: "050.xxx.xxx-**" },
+      { name: "BRUNA BEATRIZ GONCALVES REIS", cpf: "078.xxx.xxx-**" },
+      { name: "GABRIELE RODRIGUES CASTRO", cpf: "707.xxx.xxx-**" },
+      { name: "HELLEN KHAYLLEM ARAÚJO MELO", cpf: "082.xxx.xxx-**" },
+      { name: "JACINARA SILVA AMORIM", cpf: "105.xxx.xxx-**" },
+      { name: "JAKSON COSTA RODRIGUES", cpf: "634.xxx.xxx-**" },
+      { name: "JOÃO GABRIEL DE DINIZ BUAES ", cpf: "615.xxx.xxx-**" },
+      { name: "JOÃO KELVYN SANTOS MELO ", cpf: "606.xxx.xxx-**" },
+      { name: "JOAO RICARDO LOPES ", cpf: "083.xxx.xxx-**" },
+      { name: "JOERBSON SEBASTIAM PEREIRA NOGUEIRA", cpf: "606.xxx.xxx-**" },
+      { name: "LAUANE VITÓRIA ABREU FRANÇA", cpf: "106.xxx.xxx-**" },
+      { name: "MAKSUEL PEREIRA SANTOS ", cpf: "630.xxx.xxx-**" },
+      { name: "RONIELSON LEITE CASTRO", cpf: "633.xxx.xxx-**" },
+      { name: "RUAN PABLO BOAES PEREIRE", cpf: "617.xxx.xxx-**" },
+      { name: "THAIS MARTINS CAMPOS ", cpf: "615.xxx.xxx-**" },
+      { name: "THAISSA MARTINS CAMPOS ", cpf: "615.xxx.xxx-**" },
+      { name: "THAYLANNE DOS INOCENTES ARAÚJO", cpf: "082.xxx.xxx-**" },
+      { name: "TIAGO ALVES CANTANHEDE ", cpf: "707.xxx.xxx-**" },
+      { name: "WILLIAM DE JESUS FURTADO BOAES ", cpf: "622.xxx.xxx-**" },
     ]
   },
   links: {
@@ -33,6 +52,10 @@ const trabalhoJovem = {
   docs: {
     title: "Documentos e Arquivos",
     items: [
+      {
+        label: "Lista de Jovens Sorteados em 2026",
+        href: "public/trabalho-jovem/arquivos/lista-jovens-sorteados-2026.pdf"
+      },
       {
         label: "Edital Normativo No. 01/2026",
         href: "https://trabalhojovem.ma.gov.br/wp-content/uploads/2026/03/Instrucao-Normativa-No-01_2026-SEINC.pdf"
